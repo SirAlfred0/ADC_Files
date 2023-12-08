71434da03182d5609f7b2a0a23670a8644002993
SirAlfred0 <82746020+SirAlfred0@users.noreply.github.com>
2023-12-08 09:40:51 +0330

    Update date-formatter-english.ts

--- date-formatter-english.ts
+++ date-formatter-english.ts
@@ -1,10 +1,10 @@
 import { UCIDateFormatter, UCDateSplitter } from "universal-components";
 
-export class DateFormatterEnglish implements JCIDateFormatter
+export class DateFormatterEnglish implements UCIDateFormatter
 {
     //Different calendar types use different splitters to split Year, month, and day from each other change this to match your calendar type
-    get DateSplitter(): JCDateSplitter {
-        return JCDateSplitter.hyphen
+    get DateSplitter(): UCDateSplitter {
+        return UCDateSplitter.hyphen
     }
 
 }
