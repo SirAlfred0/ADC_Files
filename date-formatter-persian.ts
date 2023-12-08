86290c1efc28f41440bdc793d16344b48127062c
SirAlfred0 <82746020+SirAlfred0@users.noreply.github.com>
2023-12-08 18:07:04 +0330

    Update date-formatter-persian.ts

--- date-formatter-persian.ts
+++ date-formatter-persian.ts
@@ -1,10 +1,10 @@
-import { UCIDateFormatter, UCDateSplitter } from "universal-components";
+import { ADCIDateFormatter, ADCDateSplitter } from "@angular-date-components/scheduler";
 
-export class DateFormatterPersian implements UCIDateFormatter
+export class DateFormatterPersian implements ADCIDateFormatter
 {
     //Different calendar types use different splitters to split Year, month, and day from each other change this to match your calendar type
-    get DateSplitter(): UCDateSplitter {
-        return UCDateSplitter.slash
+    get DateSplitter(): ADCDateSplitter {
+        return ADCDateSplitter.slash
     }
 
 }
