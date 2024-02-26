ed6cf5b84c83f9402239e29ffc0e094421a6e1b4
SirAlfred0 <82746020+SirAlfred0@users.noreply.github.com>
2024-02-26 19:14:19 +0330

    Update date-formatter-english.ts

--- date-formatter-english.ts
+++ date-formatter-english.ts
@@ -2,6 +2,7 @@ import { ADCIDateFormatter, ADCDateSplitter } from "@asadi/angular-date-componen
 
 export class DateFormatterEnglish implements ADCIDateFormatter
 {
+    //Different calendar types use different splitters to split Year, month, and day from each other change this to match your calendar type
     get DateSplitter(): ADCDateSplitter {
         return ADCDateSplitter.hyphen
     }
